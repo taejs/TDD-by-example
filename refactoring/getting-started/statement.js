e48b61d29cc50c50a5ba8ab89f6da197e772dffd
taerim.shin <taerim.shin@navercorp.com>
2021-04-21 02:05:00 +0900

    [6] 표 비용 계산 로직 분리

--- refactoring/getting-started/statement.js
+++ refactoring/getting-started/statement.js
@@ -1,6 +1,7 @@
 const amountFor = require("./amoutFor");
+const volumeCreditsFor = require("./volumeCreditsFor");
 
-function statement(invoice, plays) {
+function statement(invoice) {
   let totalAmount = 0;
   let volumeCredits = 0;
   let result = `청구 내역 : 고객명 ${invoice.customer}\n`;
@@ -11,13 +12,12 @@ function statement(invoice, plays) {
   }).format;
 
   for (let perf of invoice.performances) {
-    volumeCredits += Math.max(perf.audience - 30, 0);
-    if ("comedy" === playFor(perf).type)
-      volumeCredits += Math.floor(perf.audience / 5);
+    volumeCredits += volumeCreditsFor(perf);
 
     result += `${playFor(perf).name}: ${format(amountFor(perf) / 100)} (${
       perf.audience
     }석)\n`;
+
     totalAmount += amountFor(perf);
   }
   result += `총액 ${format(totalAmount / 100)}\n`;
---
+++ refactoring/getting-started/volumeCreditsFor.js
@@ -0,0 +1,12 @@
+function volumeCreditsFor(perf) {
+  let volumeCredits = 0;
+  volumeCredits += Math.max(perf.audience - 30, 0);
+
+  if ("comedy" === play.type) {
+    volumeCredits += Math.floor(perf.audience / 5);
+  }
+
+  return volumeCredits;
+}
+
+module.exports = volumeCreditsFor;
