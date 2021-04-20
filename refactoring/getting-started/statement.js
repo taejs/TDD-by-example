f72afa21b003cfda95165d07479506c04c86d833
taerim.shin <taerim.shin@navercorp.com>
2021-04-21 02:06:05 +0900

    [7] formatting 지역 변수 삭제 - usd 함수에서 포맷팅 및 연산 수행

--- refactoring/getting-started/statement.js
+++ refactoring/getting-started/statement.js
@@ -1,26 +1,29 @@
 const amountFor = require("./amoutFor");
 const volumeCreditsFor = require("./volumeCreditsFor");
 
+function usd(amount) {
+  return new Intl.NumberFormat("en-US", {
+    style: "currency",
+    currency: "USD",
+    minimumFractionDigits: 2
+  }).format(amount / 100);
+}
+
 function statement(invoice) {
   let totalAmount = 0;
   let volumeCredits = 0;
   let result = `청구 내역 : 고객명 ${invoice.customer}\n`;
-  const format = new Intl.NumberFormat("en-US", {
-    style: "currency",
-    currency: "USD",
-    minimumFractionDigits: 2
-  }).format;
 
   for (let perf of invoice.performances) {
     volumeCredits += volumeCreditsFor(perf);
 
-    result += `${playFor(perf).name}: ${format(amountFor(perf) / 100)} (${
+    result += `${playFor(perf).name}: ${usd(amountFor(perf))} (${
       perf.audience
     }석)\n`;
 
     totalAmount += amountFor(perf);
   }
-  result += `총액 ${format(totalAmount / 100)}\n`;
+  result += `총액 ${usd(totalAmount)}\n`;
   result += `적립 포인트: ${volumeCredits}점\n`;
   return result;
 }
