625abaa3dbcbca81df6d03941dc32cbdcf9fb9cc
taejs <taeshindev@gmail.com>
2021-05-04 11:23:13 +0900

    리팩토링 8장 - 다른 클래스로 기능 이동 : AccountType으로 기능 위임

--- refactoring/moving-features/move-function/class.ts
+++ refactoring/moving-features/move-function/class.ts
@@ -1,5 +1,6 @@
 class Account {
   _daysOverdrawn: number;
+  type = new AccountType();
 
   get bankCharge() {
     // 은행 이자 계산
@@ -8,24 +9,9 @@ class Account {
     return result;
   }
 
-  get type() {
-    return {
-      isPremium: true
-    };
-  }
-
   get overdraftCharge() {
-    //초과 인출 이자 계산
-    if (this.type.isPremium) {
-      const baseCharge = 10;
-      if (this._daysOverdrawn <= 7) {
-        return baseCharge;
-      } else {
-        return baseCharge + (this._daysOverdrawn - 7) * 0.85;
-      }
-    } else {
-      return this._daysOverdrawn * 1.75;
-    }
+    // 위임 메서드
+    return this.type.overdraftCharge(this._daysOverdrawn);
   }
 }
 
