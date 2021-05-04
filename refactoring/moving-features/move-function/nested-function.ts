2def41c53bd61dbe64bdc9049fd253f27411486f
taejs <taeshindev@gmail.com>
2021-05-04 10:36:29 +0900

    리팩토링 8장 - 함수 옮기기 : 테스트 통과 후 함수 대체

--- refactoring/moving-features/move-function/nested-function.ts
+++ refactoring/moving-features/move-function/nested-function.ts
@@ -1,35 +1,18 @@
 function trackSummary(points) {
   const totalTime = calculateTime();
-  const totalDistance = calculateDistance();
-  const pace = totalTime / 60 / totalDistance;
+  const pace = totalTime / 60 / totalDistances(points);
   return {
     time: totalTime,
-    distance: totalDistance,
+    distance: totalDistances(points),
     pace
   };
-
-  function calculateDistance() {
-    let result = 0;
-    for (let i = 1; i < points.length; i++) {
-      result += distance(points[i - 1], points[i]);
-    }
-    return result;
-  }
-
-  function distance(p1, p2) {
-    return radians(0);
-  }
-
-  function radians(degrees) {
-    return 0;
-  }
 
   function calculateTime() {
     return 0;
   }
 }
 
-function temp_calculateDistance(points) {
+function totalDistances(points) {
   let result = 0;
   for (let i = 1; i < points.length; i++) {
     result += distance(points[i - 1], points[i]);
