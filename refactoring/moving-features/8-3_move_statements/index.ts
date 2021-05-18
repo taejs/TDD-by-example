fe12b689a1e0c4d4fe153d184ed895206afeff04
taejs <taeshindev@gmail.com>
2021-05-18 11:35:51 +0900

    리팩토링 8장 - 문장을 함수로 옮기기 - 함수 이름 원복

--- refactoring/moving-features/8-3_move_statements/index.ts
+++ refactoring/moving-features/8-3_move_statements/index.ts
@@ -10,19 +10,19 @@ type TPerson = {
 };
 
 function photoDiv(photo: TPhoto) {
-  return ["<div>", tempDiv(photo), "</div>"];
+  return ["<div>", emitPhotoData(photo), "</div>"];
 }
 
 function renderFunction(outStream, person: TPerson) {
   const result = [];
   result.push(`<p>${person.name}</p>`);
   result.push(`<img src="${person.photo}"/>`);
-  result.push(tempDiv(person.photo));
+  result.push(emitPhotoData(person.photo));
 
   return result.join("\n");
 }
 
-function tempDiv(photo: TPhoto) {
+function emitPhotoData(photo: TPhoto) {
   return [
     `<p>제목 : ${photo.title}</p>`, //공통
     `<p>위치 : ${photo.location}</p>`,
