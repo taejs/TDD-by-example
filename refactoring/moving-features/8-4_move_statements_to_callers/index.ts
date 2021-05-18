3ba2cca15aee0e1f09f102c36004911d938d4b92
taejs <taeshindev@gmail.com>
2021-05-18 11:51:03 +0900

    리팩토링 8장 - 문장을 함수로 옮기기 - 기존 함수 삭제

--- refactoring/moving-features/8-4_move_statements_to_callers/index.ts
+++ refactoring/moving-features/8-4_move_statements_to_callers/index.ts
@@ -20,7 +20,8 @@ function renderPhoto(outStream: TStream, photo: TPhoto) {
 function renderPerson(outStream: TStream, person: TPerson) {
   outStream.write(`<p>${person.name}</p>`);
   renderPhoto(outStream, person.photo);
-  emitPhotoData(outStream, person.photo);
+  tempPhotoData(outStream, person.photo);
+  outStream.write(`<p>위치 : ${person.photo.location}</p>`); // 위치 정보에 대해 변경 요청
 }
 
 function recentDateCutoff() {
@@ -32,13 +33,14 @@ function listRecentPhotos(outStream: TStream, photos: TPhoto[]) {
     .filter(p => p.date > recentDateCutoff())
     .forEach(p => {
       outStream.write("<div>\n");
-      emitPhotoData(outStream, p);
+      tempPhotoData(outStream, p);
+      outStream.write(`<p>위치 : ${p.location}</p>`); // 위치 정보에 대해 변경 요청
       outStream.write("</div>\n");
     });
 }
 
-function emitPhotoData(outStream: TStream, photo: TPhoto) {
+function tempPhotoData(outStream: TStream, photo: TPhoto) {
+  // 이동하지 않을 코드
   outStream.write(`<p>제목 : ${photo.title}</p>`);
   outStream.write(`<p>날짜 : ${photo.date.toString()}</p>`);
-  outStream.write(`<p>위치 : ${photo.location}</p>`); // 위치 정보에 대해 변경 요청
 }
