da426081f4531141c9cf039a124a26bc10cf1b54
taejs <taeshindev@gmail.com>
2021-05-25 03:23:50 +0900

    9장 파생 변수를 질의 함수로 바꾸기 - 예시

--- refactoring/organazing-data/3.replace-derived-variable-with-query/index.ts
+++ refactoring/organazing-data/3.replace-derived-variable-with-query/index.ts
@@ -6,10 +6,9 @@ class ProductionPlan {
   _production;
   _adjustments: Adjustment[] = [];
   get production() {
-    return this._production;
+    return this._adjustments.reduce((acc, cur) => acc + cur.amount, 0);
   }
   applyAdjustment(anAdjustment: Adjustment) {
     this._adjustments.push(anAdjustment);
-    this._production += anAdjustment.amount;
   }
 }
