c5eab16b12a24c894c8d733ce255c8044ad01c50
taejs <taeshindev@gmail.com>
2021-05-25 02:24:36 +0900

    9장 변수 쪼개기

--- refactoring/organazing-data/1.split-variable/index.ts
+++ refactoring/organazing-data/1.split-variable/index.ts
@@ -1,18 +1,26 @@
+function calculateDistance(acc, time) {
+  return 0.5 * acc * Math.pow(time, 2);
+}
+
 function distanceTravelled(scenario, time) {
-  let result;
-  let acc = scenario.primaryForce / scenario.mass;
-  let primaryTime = Math.min(time, scenario.delay);
-  let secondaryTime = time - scenario.delay;
-  result = 0.5 * acc * primaryTime * primaryTime;
+  const primaryAcceleration = scenario.primaryForce / scenario.mass;
+  const primaryTime = Math.min(time, scenario.delay);
+  const secondaryTime = time - scenario.delay;
+  const primaryDistance = calculateDistance(primaryAcceleration, primaryTime);
 
   if (secondaryTime > 0) {
-    let primaryVelocity = acc * scenario.delay;
-    acc = (scenario.primaryForce + scenario.secondaryForce) / scenario.mass;
-    result +=
+    const primaryVelocity = primaryAcceleration * scenario.delay;
+    const secondaryAcceleration =
+      (scenario.primaryForce + scenario.secondaryForce) / scenario.mass;
+
+    return (
+      primaryDistance +
       primaryVelocity * secondaryTime +
-      0.5 * acc * secondaryTime * secondaryTime;
+      calculateDistance(secondaryAcceleration, secondaryTime)
+    );
   }
-  return result;
+
+  return primaryDistance;
 }
 
 // acc에 값을 2번 할당함 (역할이 2개) -> 쪼개야 함
